5ddd330e35388c45f3847abac15f066003913d82
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-03-29 22:49:02 -0700

    added buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,6 +8,7 @@ export interface ButtonProps {
   primary?: boolean
   transparent?: boolean
   blackFont?: boolean
+  fontOverride?: string
 }
 
 export const Button = styled.button<ButtonProps>`
@@ -15,9 +16,19 @@ export const Button = styled.button<ButtonProps>`
   background: ${props => props.primary ? `${colors.black}`:props.transparent? "none": `${colors.white}`};
   color: ${props => props.primary||(props.transparent && !props.blackFont) ? "white" : `${colors.black}`};
 
-  font-size: 1.625em;
+  font-size: ${props => props.fontOverride? `${props.fontOverride}`: "1.625em"};
   margin: 1em;
   padding: 0.5em 1em;
   border: 2px solid #E1A87A;
   border-radius: 0px;
+  transition: letter-spacing 0.5s;
+  &:hover,
+  &:active,
+  &:focus {
+    cursor: pointer;
+    letter-spacing: 2px;
+  }
+  &:focus {
+    outline: none;
+  }
 `;
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from "../components/Button";
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { Button } from '../components/Button'
 
 export default {
   title: 'Button',
   component: Button,
-};
+}
 // stoires
-export const Default = () => <Button onClick={action('clicked')}>Default Button</Button>;
-export const Primary = () => <Button primary onClick={action('clicked')}>Primary Button</Button>;
-export const TransparentBackground = () => <Button transparent onClick={action('clicked')}>Transparent Background</Button>
-export const TransparentBackgroundBlackFont = () => <Button transparent blackFont onClick={action('clicked')}>Transparent Background Black Font</Button>
+export const Default = () => <Button onClick={action('clicked')}>Default Button</Button>
+export const Primary = () => (
+  <Button primary onClick={action('clicked')}>
+    Primary Button
+  </Button>
+)
+export const Background = () => (
+  <div>
+    <Button transparent onClick={action('clicked')}>
+      Transparent Background
+    </Button>
+    <Button transparent blackFont onClick={action('clicked')}>
+      Transparent Background Black Font
+    </Button>
+  </div>
+)
+
+export const CustomFont = () => (
+  <div>
+    <Button fontOverride={'0.5rem'} onClick={action('clicked')}>
+      0.5rem
+    </Button>
+    <Button fontOverride={'1rem'} onClick={action('clicked')}>
+      1rem
+    </Button>
+    <Button fontOverride={'2rem'} onClick={action('clicked')}>
+      2rem
+    </Button>
+    <Button fontOverride={'3rem'} onClick={action('clicked')}>
+      3rem
+    </Button>
+  </div>
+)
