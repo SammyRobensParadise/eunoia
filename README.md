66c765ae6d1b58a02df2cfadb31c77841238a366
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-03-29 23:19:47 -0700

    updated readme

--- README.md
+++ README.md
@@ -8,11 +8,32 @@
 ![Version](https://img.shields.io/npm/v/eunoia?style=for-the-badge)
 
 ## Storybook Link
-coming soon
+![Storybook](https://user-images.githubusercontent.com/321738/63501763-88dbf600-c4cc-11e9-96cd-94adadc2fd72.png)
+[Storybook - Eunoia](https://confident-morse-bbc2f8.netlify.com/?path=/story/button--default)
 
+## NPM Link
+![NPM](https://avatars0.githubusercontent.com/u/6078720?s=200&v=4)\
+[NPM eunoia](https://www.npmjs.com/package/eunoia)
+
+## Setup
+Install eunoia
+```
+npm i eunoia
+```
 ## What is available
-coming soon
+### Button
+```jsx
+const Default = () => <Button onClick={action('clicked')}>Default Button</Button>
+```
+### Button Config
+```ts
+  primary?: boolean
+  transparent?: boolean
+  blackFont?: boolean
+```
 
+### More coming soon
+:)
 ## Contact
 srobensparadise@gmail.com \
 https://sammyrp.com
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from "../components/Button";
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { Button } from '../components/Button'
 
 export default {
   title: 'Button',
   component: Button,
-};
+}
 // stoires
-export const Default = () => <Button onClick={action('clicked')}>Default Button</Button>;
-export const Primary = () => <Button primary onClick={action('clicked')}>Primary Button</Button>;
-export const TransparentBackground = () => <Button transparent onClick={action('clicked')}>Transparent Background</Button>
-export const TransparentBackgroundBlackFont = () => <Button transparent blackFont onClick={action('clicked')}>Transparent Background Black Font</Button>
+export const Default = () => <Button onClick={action('clicked')}>Default Button</Button>
+export const Primary = () => (
+  <Button primary onClick={action('clicked')}>
+    Primary Button
+  </Button>
+)
+export const TransparentBackground = () => (
+  <Button transparent onClick={action('clicked')}>
+    Transparent Background
+  </Button>
+)
+export const TransparentBackgroundBlackFont = () => (
+  <Button transparent blackFont onClick={action('clicked')}>
+    Transparent Background Black Font
+  </Button>
+)
