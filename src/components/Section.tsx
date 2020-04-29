c3cc72af0b5dacee3157bd33931af18c2fac4aa6
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-04-29 14:25:13 -0700

    added Section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,54 +1,39 @@
-import React from 'react'
-import {
-  Container,
-  Card as CardUI,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-} from '@material-ui/core'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
-import { BrowserRouter as Router, NavLink } from 'react-router-dom'
 import { UIStyle } from '../constants/constants'
 import styled from 'styled-components'
 
-interface SectionProps {
+interface SectionHeaderProps {
   config: {
     shouldRender: boolean | undefined
-    imageURL?: string | undefined
-    imageAltText?: string | undefined
-    content?: string
-    title: string
-    centerTitle?: boolean | undefined
-    centerContent?: boolean | undefined
+    text?: string
+    centerText?: boolean | undefined
     fontOverride?: string | undefined
     fontColor?: string | undefined
-    fontSize?: string | undefined
-    fontSizeTitle?: string | undefined
-    height?: string | number | undefined
-    link?: string | undefined | any
-    event?: any | undefined
-    containImage?: boolean | undefined
+    fontSize?: number | undefined
+    height?: number | undefined
+    backgroundColor?: string | undefined
   }
 }
 
-interface SectionState {
-  shouldRender: boolean | undefined
-}
-
-export class Section extends React.PureComponent<SectionProps, SectionState> {
-  static defaultProps = {
-    activeSection: null,
-  }
-  constructor(props: SectionProps) {
-    super(props)
-
-    this.state = {
-      shouldRender: this.props.config.shouldRender,
+export const SectionHeader = styled.div<SectionHeaderProps>`
+  width: auto;
+  height: ${(p) => (p.config.height ? `${p.config.height}px` : '220px')};
+  background-color: ${(p) => (p.config.backgroundColor ? p.config.backgroundColor : 'none')};
+  color: ${(p) => (p.config.fontColor ? p.config.fontColor : UIStyle.UIColors.black)};
+  text-align: ${(p) => (p.config.centerText ? 'center' : 'left')};
+  font-family: ${(p) => (p.config.fontOverride ? p.config.fontOverride : 'Arial')};
+  text-decoration: none;
+  font-size: ${(p) => (p.config.fontSize ? `${p.config.fontSize}px` : '50px')};
+  & {
+    @media screen and (max-width: 800px) {
+      font-size: ${(p) => (p.config.fontSize ? `${p.config.fontSize - 20}px` : '50px')};
+      height: ${(p) => (p.config.height ? `${p.config.height - 80}px` : '170px')};
+    }
+    @media screen and (max-width: 400px) {
+      font-size: ${(p) =>
+        p.config.fontSize
+          ? `${p.config.fontSize - 40 < 18 ? 18 : p.config.fontSize - 40}px`
+          : '50px'};
+      height: ${(p) => (p.config.height ? `${p.config.height - 100}px` : '170px')};
     }
   }
-  render() {
- return (<div>
-     <p>section</p>
- </div>)
-}
-}
+`
--- src/stories/Section.stories.tsx
+++ src/stories/Section.stories.tsx
@@ -1,45 +1,68 @@
 import React from 'react'
-import { Section } from '../components/Section'
+import { SectionHeader } from '../components/Section'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { styleConfig } from '../utils/config'
-// import { UIStyle } from '../constants/constants'
+import { UIStyle } from '../constants/constants'
 import '../css/styles/styles.css'
-
+import styled from 'styled-components'
 // constants
 const stories = storiesOf('Components', module)
 const styles = styleConfig.styles
 const config = {
   shouldRender: true,
-  content: `AI for better vision helping people see more every day, improving their lives`,
-  title: `Intensif-Eye; Google Vision AI, Design`,
-  imageURL: 'https://i.ibb.co/r2NBmKn/intensif-eye-render-shadow-4.png',
+  text: 'title',
+  centerText: true,
   fontOverride: 'Georgia Regular',
-  fontSize: '18px',
-  fontSizeTitle: '30px',
-  link: '/#',
+  fontColor: UIStyle.UIColors.gold,
+  fontSize: 50,
+  height: 220,
+  backgroundColor: UIStyle.UIColors.darkGrey,
 }
+const Content = styled.h3`
+  margin: 0 auto;
+  height: inherit;
+  padding-top: 15vh;
+  &{
+    @media screen and (max-width: 800px){
+      padding-top: 10vh;
+    }
+    @media screen and (max-width: 400px){
+      padding-top: 10vh;
+    }
+  }
+`
 stories.add(
-  'Section',
+  'SectionHeader',
   withInfo({
     styles,
     text: `
-   ### Designed as
+   ### Designed as section header component
    ### Usage:
     ~~~jsx
-    <Card config={config} />
+    <SectionHeader config={config} />
     ~~~
     ### Config:
     ~~~ts
     config: {
+      shouldRender: boolean | undefined
+      text?: string
+      centerText?: boolean | undefined
+      fontOverride?: string | undefined
+      fontColor?: string | undefined
+      fontSize?: number | undefined
+      height?: number | undefined
+      backgroundColor?: string | undefined
     }
     ~~~
     `,
     inline: true,
-    source: false,
+    source: true,
   })(() => (
     <div>
-      <Section config={config} />
+      <SectionHeader config={config}>
+        <Content>Design</Content>
+      </SectionHeader>
     </div>
   )),
 )
