7fdcbef9f8d8dc6dde6b7c3a8ed2aac233bd5c3d
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-04-28 21:53:26 -0700

    added linking for router

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ import {
   CardMedia,
   Button,
 } from '@material-ui/core'
-//import { BrowserRouter as Router, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, NavLink } from 'react-router-dom'
 //import { UIStyle } from '../constants/constants'
 //import styled from 'styled-components'
 
@@ -16,15 +16,41 @@ interface CardProps {
   config: {
     shouldRender: boolean | undefined
     imageURL?: string | undefined
+    imageAltText?: string | undefined
     content?: string
     title: string
     centerTitle?: boolean | undefined
     centerContent?: boolean | undefined
+    fontOverride?: string | undefined
+    fontColor?: string | undefined
+    fontSize?: string | undefined
+    height?: string | undefined
+    link?: string | undefined | any
   }
 }
 
 interface CardState {
   shouldRender: boolean | undefined
+}
+const CardEl = ({ config }: CardProps) => {
+  const { content, title, imageURL, height, imageAltText } = config
+  return (
+    <CardUI>
+      <CardActionArea>
+        <CardContent>
+          <h1>{title}</h1>
+          {content}
+        </CardContent>
+        <CardMedia
+          title={title}
+          image={imageURL}
+          component="img"
+          alt={imageAltText}
+          height={height}
+        />
+      </CardActionArea>
+    </CardUI>
+  )
 }
 export class Card extends React.PureComponent<CardProps, CardState> {
   static defaultProps = {
@@ -39,33 +65,24 @@ export class Card extends React.PureComponent<CardProps, CardState> {
   }
   render() {
     const { shouldRender } = this.state
-    const { content, title, imageURL } = this.props.config
-    return shouldRender ? (
-      <Container>
-        <CardUI>
-          <CardActionArea>
-            <CardContent>
-              <h1>{title}</h1>
-              {content}
-            </CardContent>
-            <CardMedia
-              title="Contemplative Reptile"
-              image={imageURL}
-              component="img"
-              alt="Contemplative Reptile"
-              height="500"
-            />
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-            </Button>
-            <Button size="small" color="primary">
-              Learn More
-            </Button>
-          </CardActions>
-        </CardUI>
-      </Container>
-    ) : null
+    const { link } = this.props.config
+    const { config } = this.props
+    if (link) {
+      return shouldRender ? (
+        <Container>
+          <Router>
+            <NavLink to={link}>
+              <CardEl config={config} />
+            </NavLink>
+          </Router>
+        </Container>
+      ) : null
+    } else {
+      return shouldRender ? (
+        <Container>
+          <CardEl config={config} />
+        </Container>
+      ) : null
+    }
   }
 }
