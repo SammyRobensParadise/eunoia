9621b75dcd42baa2625108820e5836b1c3909d1e
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-04-30 18:27:56 -0700

    version bumps

--- src/stories/Section.stories.tsx
+++ src/stories/Section.stories.tsx
@@ -11,7 +11,6 @@ const stories = storiesOf('Components', module)
 const styles = styleConfig.styles
 const config = {
   shouldRender: true,
-  text: 'title',
   centerText: true,
   fontOverride: 'Georgia Regular',
   fontColor: UIStyle.UIColors.gold,
@@ -40,13 +39,12 @@ stories.add(
    ### Designed as section header component
    ### Usage:
     ~~~jsx
-    <SectionHeader config={config} />
+    <SectionHeader config={config}>Content</SectionHeader>
     ~~~
     ### Config:
     ~~~ts
     config: {
       shouldRender: boolean | undefined
-      text?: string
       centerText?: boolean | undefined
       fontOverride?: string | undefined
       fontColor?: string | undefined
