fba2a3953ddd3a496f481ff08bd0487d30028895
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-04-30 21:22:05 -0700

    version bumps

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -23,16 +23,14 @@ export const SectionHeader = styled.div<SectionHeaderProps>`
   font-family: ${(p) => (p.config.fontOverride ? p.config.fontOverride : 'Arial')};
   text-decoration: none;
   font-size: ${(p) => (p.config.fontSize ? `${p.config.fontSize}px` : '50px')};
-  & {
-    @media screen and (max-width: 800px) {
-      height: ${(p) => (p.config.height ? `${p.config.height - 80}px` : '170px')};
-    }
-    @media screen and (max-width: 400px) {
-      font-size: ${(p) =>
-        p.config.fontSize
-          ? `${p.config.fontSize - 40 < 18 ? 18 : p.config.fontSize - 40}px`
-          : '50px'};
-      height: ${(p) => (p.config.height ? `${p.config.height - 100}px` : '170px')};
-    }
+  @media screen and (max-width: 800px) {
+    height: ${(p) => (p.config.height ? `${p.config.height - 80}px` : '170px')};
+  }
+  @media screen and (max-width: 400px) {
+    font-size: ${(p) =>
+      p.config.fontSize
+        ? `${p.config.fontSize - 40 < 18 ? 18 : p.config.fontSize - 40}px`
+        : '50px'};
+    height: ${(p) => (p.config.height ? `${p.config.height - 100}px` : '170px')};
   }
 `
